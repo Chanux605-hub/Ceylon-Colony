06920b0d17edb5bcdc650aa2dec5d23006030181
Dinuja Werake <IT23541566@my.sliit.lk>
2025-09-01 15:48:07 +0530

    main complete

--- Backend/index.js
+++ Backend/index.js
@@ -11,8 +11,8 @@ const allowOrigins = ["http://localhost:5173"];
 
 // Middleware
 app.use(cors({ origin: allowOrigins }));
-//app.use(express.json({ limit: "50mb" }));               
-//app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "50mb" }));               
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 // Routes
 app.use("/api/farms", farmRouter);
